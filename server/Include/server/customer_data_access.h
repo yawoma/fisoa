893b756ce9b36119ba92fcdcef6ff68f4b7cd056
Edem Manyo <edem.manyo1@outlook.fr>
2026-01-23 19:43:39 +0100

    Implementing customer data access object

---
+++ server/Include/server/customer_data_access.h
@@ -0,0 +1,14 @@
+#ifndef CUSTOMER_DATA_ACCESS_H
+#define CUSTOMER_DATA_ACCESS_H
+
+// NOLINTNEXTLINE(cppcoreguidelines-special-member-functions)
+class CustomerDataAccess
+{
+private:
+    /* data */
+public:
+    CustomerDataAccess(/* args */);
+    ~CustomerDataAccess();
+};
+
+#endif    // CUSTOMER_DATA_ACCESS_H
