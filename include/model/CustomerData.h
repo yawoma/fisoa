3edc6ae57469b9ffe4fd31ee33a13c944483f10f
Edem Manyo <edem.manyo1@outlook.fr>
2026-03-27 21:01:48 +0100

    Updated

--- include/model/CustomerData.h
+++ include/model/CustomerData.h
@@ -11,9 +11,16 @@ using json_t = nlohmann::json;
 class CustomerData
 {
     public:
+    // Get
     static json_t getAllCustomers();
-    static json_t getCustomer(/*customer id*/);
-    static json_t getCustomer(std::string customerName);
+    static json_t getCustomer(int customerId); //< with Id
+    static json_t getCustomer(const std::string& firstName, const std::string& name);
+    // Post: add
+    static void postCustomer(const json_t& customer);
+    // Put: update
+    static void putCustomer(const json_t& customer);
+    // Delete
+    static void deleteCustomer(const json_t& customer);
 };
 
 #endif    // CUSTOMER_DATA_ACCESS_H
