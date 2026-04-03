c098b510ad9f81f8bc7eb30ccfc13c336d7e9c43
Edem Manyo <edem.manyo1@outlook.fr>
2026-04-03 20:23:38 +0200

    To define logger

--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -16,31 +16,5 @@ const auto FILE_MAX_SIZE = 1048576 * 3; ///< set to 3MB the max size of file bef
 const auto MAX_FILES = 3; ///< maximum number of file to reach before rotation
 
 namespace flog {
-    void set_multi_logger(spdlog::logger& multiLogger){
-        // console loger setting
-        auto console_log = std::make_shared<spdlog::sinks::stdout_color_sink_mt>(); //Args &&args...
-        console_log->set_level(spdlog::level::debug);
-        console_log->set_pattern(PATTERN_DESIGN.data());
-
-        // file logger set
-        auto file_log = std::make_shared<spdlog::sinks::basic_file_sink_mt>(LOG_FILE_DIR.data()); //spdlog::sinks::rotating_file_sink_mt
-        file_log->set_level(spdlog::level::trace);
-        file_log->set_pattern(PATTERN_DESIGN.data());
-    
-        multiLogger = spdlog::logger("fisoa_multi_sinks", {console_log, file_log});
-        // return logger;
-    }
-
-    void trace(const std::string &message){
-        // spdlog::logger logger("");
-        // set_multi_logger(logger);
-        // logger.trace(message);
-        spdlog::trace(message);
-    }
-
-    void debug(const std::string& message){
-        spdlog::logger logger("");
-        set_multi_logger(logger);
-        logger.debug(message);
-    }
+    Logger::Logger(){}
 }
