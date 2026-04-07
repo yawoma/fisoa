b71722767f1b2e059034c1cefa82ab60b0b568df
Edem Manyo <edem.manyo1@outlook.fr>
2026-04-07 21:00:05 +0200

    implementing..

--- include/utils/logger.h
+++ include/utils/logger.h
@@ -2,16 +2,31 @@
 #define LOGGER_H
 
 #include "spdlog/logger.h"
+#include <string>
 
 /*Logger wrapper based on spdlog*/
 namespace flog {
+    enum Level {
+        TRACE = 0,
+        DEBUG = 1
+    };
+
     class Logger{
         public:
-        static Logger* getInstance();
+        static Logger& getInstance();
         ~Logger();
+        Logger(const Logger&) = delete; ///< To prevent copy
+        Logger& operator=(const Logger&) = delete; ///< To prevent assignment
+        Logger(Logger&&) = delete; ///< To prevent move
+        Logger& operator=(Logger&&) = delete; ///< To prevent move assignment
+
+        void trace(const std::string& msg) const;
 
         private:
+        void init();
         Logger();
+
+        spdlog::logger m_Logger;
     };
 } // namespace logger
 #endif // LOGGER_H
--- src/utils/logger.cpp
+++ src/utils/logger.cpp
@@ -3,7 +3,7 @@
 #include "spdlog/common.h"
 #include "spdlog/logger.h"
 #include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/rotating_file_sink.h"
+// #include "spdlog/sinks/rotating_file_sink.h"
 #include "spdlog/spdlog.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include <memory>
@@ -12,9 +12,25 @@
 
 constexpr std::string_view PATTERN_DESIGN = "[customer] %v";
 constexpr std::string_view LOG_FILE_DIR = "logs/fisoa_app.log";
-const auto FILE_MAX_SIZE = 1048576 * 3; ///< set to 3MB the max size of file before rotation
-const auto MAX_FILES = 3; ///< maximum number of file to reach before rotation
+constexpr std::string_view LOGGER_NAME = "flogger_sink";
+// const int FILE_MAX_SIZE = 1048576 * 3; ///< set to 3MB the max size of file before rotation
+// const int MAX_FILES = 3; ///< maximum number of file to reach before rotation
 
 namespace flog {
-    Logger::Logger(){}
+    Logger& Logger::getInstance(){
+        static Logger instance;
+        return instance;
+    }
+
+    void Logger::init(){
+        auto console_log = std::make_shared<spdlog::sinks::stdout_color_sink_mt>(); // Args &&args...
+        console_log->set_pattern(PATTERN_DESIGN.data());
+
+        auto file_log = std::make_shared<spdlog::sinks::basic_file_sink_mt>(LOG_FILE_DIR.data(), true);
+        file_log->set_pattern(PATTERN_DESIGN.data());
+        
+        m_Logger = spdlog::logger(LOGGER_NAME.data(), {console_log, file_log});
+    }
+
+    Logger::Logger() : m_Logger(""){ init(); }
 }
