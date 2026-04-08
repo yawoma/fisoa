a6842f9a00826b37561a5373a1a77a4760062110
Edem Manyo <edem.manyo1@outlook.fr>
2026-04-08 20:19:23 +0200

    Checking for logger work

--- app/main.cpp
+++ app/main.cpp
@@ -1,23 +1,19 @@
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
 #include "utils/logger.h"
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-    std::cout << "Test the app" << '\n';
-    flog::trace("Test the file trace log");
-    flog::debug("Test the consol debug log");
-    // auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-    // console_sink->set_level(spdlog::level::warn);
-    // console_sink->set_pattern("[]"); // [multi_sink_example] [%^%l%$] %v
-
-    // auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/multisink.txt", true);
-    // file_sink->set_level(spdlog::level::trace);
-
-    // spdlog::logger logger("multi_sink", {console_sink, file_sink});
-    // logger.set_level(spdlog::level::debug);
-    // logger.warn("this should appear in both {} console and file", 2);
-    // logger.info("this message should not appear in the console, only in the file");
+    std::cout << "Test the app for " << argc << " arguments that will be ignored" << '\n';
+    for (int i = 0; i < argc; ++i) {
+        std::cout << "Argument " << i << ": " << argv[i] << '\n';
+    }
+    auto logger = flog::Logger::getInstance();
+    logger.set_level(flog::Level::DEBUG); // Set log level to INFO
+    logger.trace("This is a trace message and should not appear.");
+    logger.debug("This is a debug message and should appear.");
+    logger.info("This is an info message and should appear.");
+    logger.warn("This is a warning message and should appear.");
+    logger.error("This is an error message and should appear.");
+    logger.critical("This is a critical message and should appear.");
     return 0;
 }
