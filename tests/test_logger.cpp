92fcf9b312cdf349efca8c159b3eb03cab8514b6
Edem Manyo <edem.manyo1@outlook.fr>
2026-04-21 11:59:03 +0200

    Updated due to fixe in logger

--- tests/test_logger.cpp
+++ tests/test_logger.cpp
@@ -3,25 +3,25 @@
 #include <gtest/gtest.h>
 
 TEST(LoggerTest, trace_no_throw) {
-    EXPECT_NO_THROW(flog::get_logger().trace("This is a trace message"));
+    EXPECT_NO_THROW(get_logger().trace("This is a trace message"));
 }
 
 TEST(LoggerTest, debug_no_throw) {
-    EXPECT_NO_THROW(flog::get_logger().debug("This is a debug message"));
+    EXPECT_NO_THROW(get_logger().debug("This is a debug message"));
 }
 
 TEST(LoggerTest, info_no_throw) {
-    EXPECT_NO_THROW(flog::get_logger().info("This is an info message"));
+    EXPECT_NO_THROW(get_logger().info("This is an info message"));
 }
 
 TEST(LoggerTest, warn_no_throw) {
-    EXPECT_NO_THROW(flog::get_logger().warn("This is a warning message"));
+    EXPECT_NO_THROW(get_logger().warn("This is a warning message"));
 }
 
 TEST(LoggerTest, error_no_throw) {
-    EXPECT_NO_THROW(flog::get_logger().error("This is an error message"));
+    EXPECT_NO_THROW(get_logger().error("This is an error message"));
 }
 
 TEST(LoggerTest, critical_no_throw) {
-    EXPECT_NO_THROW(flog::get_logger().critical("This is a critical message"));
+    EXPECT_NO_THROW(get_logger().critical("This is a critical message"));
 }
