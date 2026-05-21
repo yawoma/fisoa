759b43d2a304479305a9f6b5e11bf08649d12570
Edem Manyo <edem.manyo1@outlook.fr>
2026-05-21 22:28:19 +0200

    Formated with clang format

--- include/model/customer_data_access.h
+++ include/model/customer_data_access.h
@@ -1,14 +1,14 @@
 #ifndef CUSTOMER_DATA_ACCESS_H
 #define CUSTOMER_DATA_ACCESS_H
 
+#include <SQLiteCpp/SQLiteCpp.h>
+#include <cstdint>
 #include <string>
 #include <vector>
-#include <cstdint>
-#include <SQLiteCpp/SQLiteCpp.h>
 
 struct Customer
 {
-    uint32_t m_id = 0;
+    uint32_t    m_id = 0;
     std::string m_uuid;
     std::string m_firstName;
     std::string m_name;
@@ -19,23 +19,23 @@ struct Customer
     std::string m_passportId;
 };
 
-//NOLINTNEXTLINE(cppcoreguidelines-special-member-functions)
+// NOLINTNEXTLINE(cppcoreguidelines-special-member-functions)
 class CustomerDataAccess
 {
 public:
     CustomerDataAccess();
     ~CustomerDataAccess();
 
-    bool insert_customer(const Customer& customer);
-    bool update_customer(const Customer& customer);
-    bool delete_customer(const std::string& customer_id);
-    Customer get_customer(const std::string& customer_id);
+    bool                  insert_customer(const Customer& customer);
+    bool                  update_customer(const Customer& customer);
+    bool                  delete_customer(const std::string& customer_id);
+    Customer              get_customer(const std::string& customer_id);
     std::vector<Customer> list_customers();
 
 private:
-    void init_database();
+    void             init_database();
     // void close_database();
     SQLite::Database m_db;
 };
 
-#endif // CUSTOMER_DATA_ACCESS_H
+#endif    // CUSTOMER_DATA_ACCESS_H
